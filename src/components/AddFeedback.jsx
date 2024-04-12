f4de87d7350d435061c28ab863f7b9fd758512e4
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-04-12 13:46:13 +0300

    add feedback data

--- src/components/AddFeedback.jsx
+++ src/components/AddFeedback.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "../styles/feedback.css";
 import suggestionsIcon from "../assets/shared/suggestions/icon-suggestions.svg"
+import checkIcon from "../assets/shared/icon-check.svg"
 
 const sorting = [
   "Most Upvotes",
@@ -43,7 +44,7 @@ function AddFeedback() {
         <div className="filter-list">
           <ul className="comments-filter-list bg-white w-[190px] rounded-xl ml-[2em] mt-[1em] md:m-auto md:mt-[1em]">
             {sorting.map((item, index) => (
-              <li key={index} className="text-[#647196] text-[.8em] p-[.9em]">{item}</li>
+              <li key={index} className="text-[#647196] text-[.8em] p-[.9em] flex items-center justify-between">{item}{index === 0 ? <img src={checkIcon} /> : ""}</li>
             ))}
           </ul>
         </div>
