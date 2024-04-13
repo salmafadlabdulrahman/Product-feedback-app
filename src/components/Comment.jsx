f16dbc7df1917ab8eedeef8db727f6d58d92c06c
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-04-14 00:17:56 +0300

    update comment cards styling

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,23 +1,27 @@
-function Comment({ userImg, name, userName, content, id }) {
+import "../styles/comment.css"
+
+function Comment({ commentsCount, userImg, name, userName, content, id }) {
   return (
     <div>
       <div className="comment-content">
-        <div>
+        <div className="flex items-center gap-5 mt-[2.5em]">
           <img
             src={userImg}
             alt="an image of current comment's writer"
             className="rounded-[50%] w-[50px]"
           />
 
-          <div>
-          <h4>{name}</h4>
-          <h5>{userName}</h5>
+          <div className="user-info flex items-start justify-between w-[4500px]">
+            <div className="">
+              <h4 className="text-[#3A4374] font-bold tracking-wide">{name}</h4>
+              <h5 className="text-[#647196]">@{userName}</h5>
+            </div>
+            <button className="items-start text-[#4661E6] font-semibold text-[.9em]">Reply</button>
           </div>
         </div>
 
         <div className="comment-body">
-          
-          <p>{content}</p>
+          <p className="text-[#647196] mt-4">{content}</p>
         </div>
       </div>
     </div>
--- src/pages/FeedbackDetails.jsx
+++ src/pages/FeedbackDetails.jsx
@@ -50,20 +50,25 @@ function FeedbackDetails() {
         />
       </div>
 
-      <div className="comments-container bg-white mt-6 w-[90%] m-auto rounded-lg p-[2em]">
+      <div className="comments-container bg-white mt-6 md:max-w-[750px] w-[90%] m-auto rounded-lg p-[2em]">
         <h1 className="text-[#3A4374] font-bold text-[1.5em]">
           {commentData.comments.length} Comment
           {commentData.comments.length > 1 ? "s" : ""}
         </h1>
         {commentData.comments.map((item, index) => (
-          <Comment
+          <div
+            className={commentData.comments.length > 1 && index === 0 ? "comment-container" : ""}
             key={index}
-            userImg={item.user.image}
-            name={item.user.name}
-            userName={item.user.username}
-            content={item.content}
-            id={item.id}
-          />
+          >
+            <Comment
+              commentsCount={commentData.comments.length}
+              userImg={item.user.image}
+              name={item.user.name}
+              userName={item.user.username}
+              content={item.content}
+              id={item.id}
+            />
+          </div>
         ))}
       </div>
     </div>
